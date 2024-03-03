290899dab54b00d49f133f1b315d63073e575e82
LOKESH PRAMOD JANGALE <90272774+lokeshjangle@users.noreply.github.com>
2024-03-03 19:36:53 +0530

    Delete src directory

--- src/http-quiz.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://localhost:5003/api',//OUR PROJECT API
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
